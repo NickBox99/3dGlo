6770b2367862be9659f112e5a4f8490f9c5a1f7e
Dmitry2 <nickbox99@inbox.ru>
2020-07-07 00:47:48 +0300

    Сделал слайдер для 21 урока и добавил правки для анимации

--- js/main.js
+++ js/main.js
@@ -55,7 +55,7 @@ function addModalEventListener() {
                     }
                 };
 
-                if (document.documentElement.clientHeight >= 768) {
+                if (document.documentElement.clientWidth >= 768) {
                     animated();
                 } else {
                     modalMenu.style.transform = `translate(0)`;
@@ -86,7 +86,7 @@ function addModalEventListener() {
             if (event.target.classList.contains('popup-btn')){
                 modalPopup.style.display = "block";
                 
-                if (document.documentElement.clientHeight >= 768) {
+                if (document.documentElement.clientWidth >= 768) {
                     modalPopup.style.opacity = 0;
                     let opacity = 0;
 
@@ -124,9 +124,9 @@ const tabs = function(){
     tab = document.querySelectorAll(".service-header-tab"),
     tabContent = document.querySelectorAll(".service-tab");
 
-    const toggleTabContext = function(ir){
-        for(let i=0;i< tab.length; i++){
-            if(i === ir){
+    const toggleTabContext = function(index){
+        for(let i = 0; i< tab.length; i++){
+            if(i === index){
                 tab[i].classList.add('active');
                 tabContent[i].classList.remove('d-none');
             }else{
@@ -151,9 +151,107 @@ const tabs = function(){
     });
 }
 
+//Слайдер
+const slider = function(interval = 1500){
+    const sliders = document.querySelector("#all-progects"),
+    slide = sliders.querySelectorAll(".portfolio-item"),
+    dots = sliders.querySelector(".portfolio-dots");
+
+    for(let i = 0; i < slide.length; i++)
+    {
+        const element = document.createElement('li');
+        element.classList.add('dot');
+        dots.append(element);
+    }
+
+    const dot = dots.querySelectorAll('.dot');
+    if(dot.length > 0){
+        dot[0].classList.add('dot-active');
+    }
+
+    let currenSlide = 0;
+
+    const nextSlide = function (slide, index, strClass){
+        slide[index].classList.add(strClass);
+    }
+
+    const prevSlide = function (slide, index, strClass){
+        slide[index].classList.remove(strClass);
+    }
+
+    const autoPlaySlide = function (){
+        prevSlide(slide, currenSlide, "portfolio-item-active");
+        prevSlide(dot, currenSlide, "dot-active");
+        
+        currenSlide++;
+        if(currenSlide >= slide.length){
+            currenSlide = 0;
+        }
+        nextSlide(slide, currenSlide, "portfolio-item-active");
+        nextSlide(dot, currenSlide, "dot-active");
+    }
+
+    let intervalSliders;
+
+    const startSlide = function(){
+        intervalSliders = setInterval(autoPlaySlide, interval);
+    }
+
+    const stopSlide = function(){
+        clearInterval(intervalSliders);
+    }
+
+    sliders.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if(!event.target.matches(".portfolio-btn, .dot")){
+            return;
+        }
+
+        event.preventDefault();
+        prevSlide(slide, currenSlide, "portfolio-item-active");
+        prevSlide(dot, currenSlide, "dot-active");
+
+        if(target.matches("#arrow-left")){
+            currenSlide--;
+        }else if(target.matches("#arrow-right")){
+            currenSlide++;
+        }else if(target.matches(".dot")){
+            dot.forEach((elem, index) => {
+                if(elem === target){
+                    currenSlide = index;
+                }
+            });
+        }
+        
+
+        if(currenSlide >= slide.length){
+            currenSlide = 0;
+        }else if(currenSlide < 0){
+            currenSlide = slide.length - 1;
+        }
+        nextSlide(slide, currenSlide, "portfolio-item-active");
+        nextSlide(dot, currenSlide, "dot-active");
+    });
+
+    startSlide();
+    sliders.addEventListener('mouseover', (event) => {
+        if(event.target.matches(".portfolio-btn, .dot")){
+            stopSlide();
+        }
+    });
+
+    sliders.addEventListener('mouseout', (event) => {
+        if(event.target.matches(".portfolio-btn, .dot")){
+            startSlide();
+        }
+    });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     timer("30 July 2020");
     addModalEventListener();
     tabs();
+    slider(1500);
 });
 
